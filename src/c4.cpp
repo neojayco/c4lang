996c384906c51577d08be9e13b7ae48288af4404
neojayco <mr.jay.co@yandex.ru>
2023-10-17 23:19:22 +1000

    for test

--- src/c4.cpp
+++ src/c4.cpp
@@ -1,14 +1,17 @@
 #include "lex/tokenizer.h"
 #include "pass/post_lex.h"
 
+static constexpr auto DEFAULT_TEST_FILEPATH = "../../rtlib/test.c4";
+
 auto main(int argc, char* argv[]) -> int {
 
+    const auto* filename = argv[1];
     if (argc != 2) {
         std::wcout << L"usage: ./c4 <filename>" << std::endl;
-        return 1;
+        filename = DEFAULT_TEST_FILEPATH;
+        //return 1;
     }
 
-    const auto filename = argv[1];
     lex::source_content src;
     if (!lex::load_file32(src, filename)) {
         std::wcout << L"Failed to load file, not found!\n";
