3e54e181762d36127542aa299205b302718135ca
neojay <mr.jay.co@yandex.ru>
2023-10-19 09:47:14 +1000

    Create operations.h
    added simple and most common binary operation and their number is their priority as well

---
+++ src/ast/operations.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "../common.h"
+
+namespace ast {
+
+    enum binop_kind : unsigned {
+        bk_add_assign,  // +=
+        bk_sub_assign,  // -=
+        bk_mul_assign,  // *=
+        bk_div_assign,  // /=
+        bk_lor,         // logical OR (||)
+        bk_land,        // logical AND (&&)
+        bk_or,          // bitwise OR (|)
+        bk_xor,         // bitwise XOR (^)
+        bk_and,         // bitwise AND (&)
+        bk_eq,          // equals (==)
+        bk_neq,         // not equals (!=)
+        bk_ge,          // greater equals (>=)
+        bk_le,          // less equals (<=)
+        bk_gt,          // greater than (>)
+        bk_lt,          // less than (<)
+        bk_add,         // +
+        bk_sub,         // -
+        bk_mod,         // %
+        bk_div,         // /
+        bk_mul,         // *
+    };
+
+}
